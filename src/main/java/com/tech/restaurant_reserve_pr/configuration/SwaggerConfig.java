e6303d9d0eb8a073de98539c2844b9a28e3adaca
mplatab <marcosplata98@gmail.com>
2024-11-06 00:06:36 -0500

    feat: add Swagger configuration

--- src/main/java/com/tech/restaurant_reserve_pr/configuration/SwaggerConfig.java
+++ src/main/java/com/tech/restaurant_reserve_pr/configuration/SwaggerConfig.java
@@ -1,4 +1,54 @@
 package com.tech.restaurant_reserve_pr.configuration;
 
-public class SwaggerConfig {
-}
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Restaurant Reserve",
+                description = "Modern restaurant reservation management system built with Spring Boot. Features real-time table management, reservations, notifications, and administrative dashboard.",
+                termsOfService = "www.restaurant-reservation.com/termsOfServices",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "John Doe",
+                        url =  "www.restaurant-reservation.com/contact",
+                        email = "john@gmail.com"
+                ),
+                license = @License(
+                        name = "Standard Software User License",
+                        url = "www.restaurant-reservation.com/license"
+                )
+
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD SERVER",
+                        url = "http://localhost:8084"
+                )
+        },
+        security = @SecurityRequirement(
+                name = "Security Token"
+        )
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access Token For My API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {}
